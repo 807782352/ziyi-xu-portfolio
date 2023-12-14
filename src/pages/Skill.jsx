63093f13687fc16a4ac81acdcae21e243ad4a43f
z297xu <kyrie807782352@gmail.com>
2023-12-14 10:30:01 -0500

    update the skill_data with color

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -104,36 +104,42 @@ const SKILL_DATA = [
     imgUrl: "https://cdn.worldvectorlogo.com/logos/spring-3.svg",
     categories: ["all", "framework", "backend"],
     id: "013",
+    color: "#72d24a",
   },
   {
     title: "React-Redux",
     imgUrl: "https://cdn.worldvectorlogo.com/logos/redux.svg",
     categories: ["all", "library", "frontend"],
     id: "014",
+    color: "#8251cf",
   },
   {
     title: "React-Router",
     imgUrl: "https://www.svgrepo.com/show/354262/react-router.svg",
     categories: ["all", "library", "frontend"],
     id: "015",
+    color: "#e5031d",
   },
   {
     title: "Mybatis-Plus",
     imgUrl: "https://mybatis.plus/img/logo.png",
     categories: ["all", "library", "backend"],
     id: "016",
+    color: "#259dff",
   },
   {
     title: "Maven",
     imgUrl: "https://www.svgrepo.com/show/373829/maven.svg",
     categories: ["all", "library", "backend"],
     id: "017",
+    color: "#f45f30",
   },
   {
     title: "Node.js",
     imgUrl: "https://www.svgrepo.com/show/303360/nodejs-logo.svg",
     categories: ["all", "library", "backend"],
     id: "018",
+    color: "#91e12d",
   },
   {
     title: "Bootstrap",
@@ -141,18 +147,21 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png",
     categories: ["all", "library", "backend"],
     id: "019",
+    color: "#8322ff",
   },
   {
     title: "Vite",
     imgUrl: "https://www.svgrepo.com/show/374167/vite.svg",
     categories: ["all", "tool", "framework", "frontend"],
     id: "020",
+    color: "#ffdf27",
   },
   {
     title: "Tailwind",
     imgUrl: "https://www.svgrepo.com/show/354431/tailwindcss-icon.svg",
     categories: ["all", "library", "frontend"],
     id: "021",
+    color: "#1ccdcb",
   },
   {
     title: "Linux Commands",
@@ -160,6 +169,7 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1727px-Tux.svg.png",
     categories: ["all", "language", "backend"],
     id: "022",
+    color: "#ffffff",
   },
   {
     title: "Pytorch",
@@ -167,6 +177,7 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/PyTorch_logo_icon.svg/1200px-PyTorch_logo_icon.svg.png",
     categories: ["all", "library", "ml"],
     id: "023",
+    color: "#ff522c",
   },
   {
     title: "Tensorflow",
@@ -174,12 +185,14 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/957px-Tensorflow_logo.svg.png",
     categories: ["all", "library", "ml"],
     id: "024",
+    color: "#ff9d21",
   },
   {
     title: "Numpy",
     imgUrl: "https://www.svgrepo.com/show/354127/numpy.svg",
     categories: ["all", "library", "ml"],
     id: "025",
+    color: "#54bce4",
   },
   {
     title: "Pandas",
@@ -187,6 +200,7 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Pandas_mark.svg/1200px-Pandas_mark.svg.png",
     categories: ["all", "library", "ml"],
     id: "026",
+    color: "#ffdf00",
   },
   {
     title: "Matplotlib",
@@ -194,12 +208,14 @@ const SKILL_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Matplotlib_icon.svg/1024px-Matplotlib_icon.svg.png",
     categories: ["all", "library", "ml"],
     id: "027",
+    color: "#9b9b9b",
   },
   {
     title: "Spring Cloud",
     imgUrl: "https://avatars.githubusercontent.com/u/8216893?s=200&v=4",
     categories: ["all", "tool", "backend"],
     id: "028",
+    color: "#76c542",
   },
   {
     title: "AWS",
@@ -207,6 +223,7 @@ const SKILL_DATA = [
       "https://static-00.iconduck.com/assets.00/aws-icon-2048x2048-ptyrjxdo.png",
     categories: ["all", "tool", "backend"],
     id: "029",
+    color: "#f4f6f9",
   },
 ];
 
