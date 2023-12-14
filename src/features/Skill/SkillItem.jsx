9cef979083937765d64d2cdeaee574cb6bfcc986
z297xu <kyrie807782352@gmail.com>
2023-12-14 10:24:55 -0500

    fix the bug in tailwindcss dynamic colors

--- src/features/Skill/SkillItem.jsx
+++ src/features/Skill/SkillItem.jsx
@@ -1,15 +1,17 @@
 export default function SkillItem({ skill }) {
   const { title, imgUrl, color } = skill;
 
+  // get dynamic styles from index.css file
+  // NOTE: **TailwindCSS doesn't allow you to generate classes dynamically**.
+  //        So use style to handle the problem instead
+  const dynamicStyles = {
+    '--gradient-start': 'black', // default starting color
+    '--gradient-end': color || 'white', // default white color if no color provided
+  };
+
   return (
-    // TODO: Change the className into a variable, and then assign to the className
     <li className="">
-      {console.log(
-        `relative flex h-full w-full flex-col  flex-wrap  items-center justify-around gap-5 rounded-3xl  py-8  before:absolute before:inset-1 before:z-10 before:rounded-3xl before:bg-slate-900 before:content-[''] after:absolute after:-top-full after:left-[-100%] after:h-[300%] after:w-[300%] after:animate-fullSpin  after:content-[''] after:bg-[conic-gradient(black,black,${color || "white"})] overflow-hidden`,
-      )}
-      <div
-        className={`relative flex h-full w-full flex-col  flex-wrap  items-center justify-around gap-5 rounded-3xl  py-8  before:absolute before:inset-1 before:z-10 before:rounded-3xl before:bg-slate-900 before:content-[''] after:absolute after:-top-full after:left-[-100%] after:h-[300%] after:w-[300%] after:animate-fullSpin  after:content-[''] after:bg-[conic-gradient(black,black,${color || "white"})] overflow-hidden`}
-      >
+      <div className="skill-item" style={dynamicStyles}>
         <img
           src={imgUrl}
           alt={title}
@@ -22,7 +24,3 @@ export default function SkillItem({ skill }) {
     </li>
   );
 }
-
-// after:bg-gradient-to-r  after:from-black after:via-black after:to-yellow-500
-
-//
--- src/ui/AnimatedBorder.jsx
+++
@@ -1,7 +0,0 @@
-export default function AnimatedBorder({children}) {
-  return (
-    <div className="w-full h-full bg-slate-50 ">
-        {children}
-    </div>
-  )
-}
