ed40d099514ce2c6544f04503c56002b5e3b8666
z297xu <kyrie807782352@gmail.com>
2023-12-19 10:30:53 -0500

    extract the verticalLine module

--- src/features/MileStone/MileStoneItem.jsx
+++ src/features/MileStone/MileStoneItem.jsx
@@ -17,48 +17,41 @@ export default function MileStoneItem({ mileStoneItem, index }) {
   return (
     <div className="grid grid-cols-9 px-2">
       {/* right container style */}
-      {index % 2 !== 0 && (
-        <div
-          id="vertical-timeline"
-          className="col-span-1 col-start-5 flex h-full w-full items-center justify-center"
-        >
-          <div className="h-full w-1 bg-orange-400"></div>
-          <div
-            className="absolute z-10 h-16 w-16 rounded-full border-2 border-orange-400 bg-white bg-contain bg-center bg-no-repeat hover:scale-150 transition ease-in-out delay-300 duration-300"
-            style={{ backgroundImage: `url(${imgUrl})` }}
-          ></div>
-        </div>
-      )}
+      {index % 2 !== 0 && <VerticalLine imgUrl={imgUrl} />}
 
       <div id="milestone-container" className={containerStyle}>
         <span className="mx-4 my-2 inline-block text-slate-300">📅 {time}</span>
 
         <div id="text-box" className={borderStyle}>
           <h2 className="text-xl font-bold ">{title}</h2>
-          <span className="text-sm italic text-slate-300 px-1">📍 {venue}</span>
-          <p className="mt-2 text-left text-slate-200 px-6">
+          <span className="px-1 text-sm italic text-slate-300">📍 {venue}</span>
+          <p className="mt-2 px-6 text-left text-slate-200">
             <ul className="list-disc">
-                {strList.map((strItem,index) => (
-                    <li key={index}>{strItem}</li>
-                ))}
+              {strList.map((strItem, index) => (
+                <li key={index}>{strItem}</li>
+              ))}
             </ul>
           </p>
         </div>
       </div>
 
       {/* left container style  */}
-      {index % 2 === 0 && (
-        <div
-          id="vertical-timeline"
-          className="col-span-1 col-start-5 flex h-full w-full items-center justify-center"
-        >
-          <div className="h-full w-1 bg-orange-400"></div>
-          <div
-            className="absolute z-10 h-16 w-16 rounded-full border-2 border-orange-400 bg-white bg-contain bg-center bg-no-repeat hover:scale-150 transition ease-in-out delay-300 duration-300"
-            style={{ backgroundImage: `url(${imgUrl})` }}
-          ></div>
-        </div>
-      )}
+      {index % 2 === 0 && <VerticalLine imgUrl={imgUrl} />}
+    </div>
+  );
+}
+
+function VerticalLine({ imgUrl }) {
+  return (
+    <div
+      id="vertical-timeline"
+      className="col-span-1 col-start-5 flex h-full w-full items-center justify-center"
+    >
+      <div className="h-full w-1 bg-orange-400"></div>
+      <div
+        className="absolute z-10 h-16 w-16 rounded-full border-2 border-orange-400 bg-white bg-contain bg-center bg-no-repeat transition delay-300 duration-300 ease-in-out hover:scale-150"
+        style={{ backgroundImage: `url(${imgUrl})` }}
+      ></div>
     </div>
   );
 }
