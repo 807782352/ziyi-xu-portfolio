62a5e80334bd28d391c8b8ed1060a93ee3b87c94
z297xu <kyrie807782352@gmail.com>
2023-12-22 16:33:32 -0500

    small change in layout

--- src/features/Navbar/NavLinkItem.jsx
+++ src/features/Navbar/NavLinkItem.jsx
@@ -2,13 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function NavLinkItem({ url, title }) {
   return (
-    <li>
-      <Link
-        to={`/${url}`}
-        className="hover:yellow-red block px-3 py-2 font-extrabold text-white transition  duration-150 ease-in-out hover:rounded-2xl hover:text-slate-900"
-      >
-        {title}
-      </Link>
+    <li className="inline-block highlight space-x-2 px-3 py-2 text-white">
+      <Link to={`/${url}`}>{title}</Link>
     </li>
   );
 }
--- src/ui/Tag.jsx
+++ src/ui/Tag.jsx
@@ -1,5 +1,5 @@
 export default function Tag({ word }) {
   return <li>
-    <span className="inline-block text-slate-300 font-bold bg-gray-700 px-2 py-1 rounded-lg hover:yellow-red hover:text-slate-900 hover:scale-110 transition-transform ease-in-out duration-300">{word}</span>
+    <span className="inline-block text-slate-300 font-bold bg-gray-700 px-2 py-1 rounded-lg highlight">{word}</span>
   </li>;
 }
