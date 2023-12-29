97f1ff89679f0496fecdb91803e0bcbc2a22c873
z297xu <kyrie807782352@gmail.com>
2023-12-29 12:56:31 -0500

    add download cv function

--- src/ui/DownloadButton.jsx
+++ src/ui/DownloadButton.jsx
@@ -1,8 +1,12 @@
+import MyResume from "/public/resume/ZiyiXuResume.pdf";
+
 export default function DownloadButton() {
   return (
-    <div className="flex justify-center gap-1">
-      <span className="material-symbols-outlined">download</span>
-      <span>Download CV</span>
-    </div>
+    <a href={MyResume} download="Resume-Ziyi-Xu">
+      <div className="flex justify-center gap-1">
+        <span className="material-symbols-outlined">download</span>
+        <span>Download CV</span>
+      </div>
+    </a>
   );
 }
